5da7b3dbf60cdb0893901201661502c0fbf4d353
SolmazShiraliyeva <149113679+SolmazShiraliyeva@users.noreply.github.com>
2024-01-23 00:49:53 +0400

    Add files via upload

---
+++ js_project02/app.js
@@ -0,0 +1,17 @@
+
+const passField = document.querySelector("#passInput");
+const showBtn = document.querySelector("span i");
+
+showBtn.onclick = (()=>{
+    if(passField.type==="password"){
+        passField.type = "text";
+        showBtn.classList.add('hide-btn');
+    }else{
+        passField.type = "password";
+        showBtn.classList.remove("hide-btn");
+    }
+});
+
+
+
+
